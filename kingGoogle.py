f8f931918cd11512f5683443cddebc952e449b2f
Michael Tanner <m.tanner161@gmail.com>
2021-12-10 12:07:43 -0700

    Running CODE!!! Added the client authentication

--- kingGoogle.py
+++ kingGoogle.py
@@ -5,11 +5,20 @@ from google.analytics.data_v1beta.types import Metric
 from google.analytics.data_v1beta.types import RunReportRequest
 
 
-def sample_run_report(property_id="YOUR-GA4-PROPERTY-ID"):
-    """Runs a simple report on a Google Analytics 4 property."""
-    # TODO(developer): Uncomment this variable and replace with your
-    #  Google Analytics 4 property ID before running the sample.
-    # property_id = "YOUR-GA4-PROPERTY-ID"
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+
+def sample_run_report(property_id="263279665"):
+    property_id = "263279665"
 
     # Using a default constructor instructs the client to use the credentials
     # specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
@@ -17,12 +26,16 @@ def sample_run_report(property_id="YOUR-GA4-PROPERTY-ID"):
 
     request = RunReportRequest(
         property=f"properties/{property_id}",
-        dimensions=[Dimension(name="city")],
         metrics=[Metric(name="activeUsers")],
-        date_ranges=[DateRange(start_date="2020-03-31", end_date="today")],
+        date_ranges=[DateRange(start_date="2021-12-09", end_date="today")],
     )
     response = client.run_report(request)
 
-    print("Report result:")
+    print("Michael result:")
     for row in response.rows:
-        print(row.dimension_values[0].value, row.metric_values[0].value)
+        numUsers = row.metric_values[0].value
+        print(numUsers)
+
+
+if __name__ == "__main__":
+    sample_run_report()
