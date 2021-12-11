e24643e572914d6f4445ba22455ae362f3605162
Michael Tanner <m.tanner161@gmail.com>
2021-12-10 17:28:14 -0700

    Added comments and change the main function name to acccount for easy of use

--- kingGoogle.py
+++ kingGoogle.py
@@ -1,10 +1,22 @@
+## Google API report that pulls all relavent data from King Operating Website
+## Developed by Michael Tanner - Sandstone Group
+
+# Google imports classes needed $ pip install google-analytics-data
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import DateRange
 from google.analytics.data_v1beta.types import Dimension
 from google.analytics.data_v1beta.types import Metric
 from google.analytics.data_v1beta.types import RunReportRequest
 
+# packages needed for the full project
+import pandas as pd
+import numpy as np
+import os
+import json
+import requests
 
+
+# intializes the Google Cloud Client
 def implicit():
     from google.cloud import storage
 
@@ -17,12 +29,10 @@ def implicit():
     print(buckets)
 
 
-def sample_run_report(property_id="263279665"):
-    property_id = "263279665"
-
-    # Using a default constructor instructs the client to use the credentials
-    # specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
-    client = BetaAnalyticsDataClient()
+# runs activeUserReport which gets total number of user-historcal
+def activeUserReport(property_id="263279665"):
+    property_id = "263279665"  # propertyID from Google Analytics
+    client = BetaAnalyticsDataClient()  # calls the authentaction token
 
     request = RunReportRequest(
         property=f"properties/{property_id}",
@@ -31,11 +41,13 @@ def sample_run_report(property_id="263279665"):
     )
     response = client.run_report(request)
 
+    # prints results
     print("Michael result:")
     for row in response.rows:
-        numUsers = row.metric_values[0].value
-        print(numUsers)
+        numViews = row.metric_values[0].value
+        print(numViews)
 
 
+# Runs the file
 if __name__ == "__main__":
-    sample_run_report()
+    activeUserReport()
